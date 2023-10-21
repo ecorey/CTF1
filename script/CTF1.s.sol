4ee4923f48f7b9ad40d5a8e26b215557e704bbba
brandon engstrom <bengstrom32@gmail.com>
2023-10-21 16:09:31 -0400

    first commit

--- script/CTF1.s.sol
+++ script/CTF1.s.sol
@@ -30,3 +30,5 @@ contract CtfDeploy is Script {
 // To Deploy and Run script just add private key with no double quotes
 // forge create --rpc-url https://sepolia.gateway.pokt.network/v1/lb/ae75c2b1 --private-key 123456jiopjoihw4twt45tyhevsssgeh5u454 src/CTF1.sol:Foo
 
+// SUBMITTED:
+// https://sepolia.etherscan.io/tx/0xbb9ff532a3445a128367f094070c7df8a8f1114e902fbc2db84b1d3f93c76795
